f9bb49c6377e85f6e968abd940cc7e3a65637faa
David Irvine <irvined@gmail.com>
2013-11-30 12:11:34 -0600

    Initial import

---
+++ setup.py
@@ -0,0 +1,34 @@
+# Copyright 2013 David Irvine
+#
+# This file is part of gridengine-accounting
+#
+# gridengine-accounting is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# gridengine-accounting is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gridengine-accounting.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+setup(
+	name="gridengine-accounting",
+	version="0.1",
+	description="Read grid engine accounting files",
+	author="David Irvine",
+	author_email="irvined@gmail.com",
+	url="https://github.com/irvined1982/gridengine-accounting",
+	packages=["gridengine.accounting"],
+	classifiers=[
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+		"Natural Language :: English",
+		"Operating System :: POSIX",
+		"Topic :: Scientific/Engineering",
+		"Topic :: Utilities",
+		],
+)
